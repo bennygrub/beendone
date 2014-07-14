98f1f1c629e58ccd386364649112f6b4c6b90212
Ben Gruber <blgruber@gmail.com>
2014-07-14 18:53:07 -0400

    added older jetblue and almost done with older united

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -255,7 +255,30 @@ class PagesController < ApplicationController
   	contextio = ContextIO.new('p3o3c7vm', '8kYkj7Qv9xKeVitj')
   	#get the correct account
   	account = contextio.accounts.where(email: 'blgruber@gmail.com').first
-  	
+  	jb_messages_old = account.messages.where(from: "mail@jetblueconnect.com", subject: "Your JetBlue E-tinerary")
+  	jb_messages_old = jb_messages_old.map {|message| message.body_parts.first.content}
+  	jb_messages_old.each do |message|
+  		dom = Nokogiri::HTML(message)
+	  	matches = dom.xpath('/html/body/div/table/tr[11]/td/table/tr').map(&:to_s)
+	  	matches.shift(2)
+	  	matches.each do |match|
+	  		flight_array = match.scan(/>(.*?)</)
+	  		date = flight_array[0].first
+	  		departure_data = flight_array[2].first	  		
+	  		depart_time = departure_data.split.pop
+	  		d_split = departure_data.split
+	  		d_split.pop
+	  		depart_airport = d_split.join(" ")
+	  		arrival_data = flight_array[3].first
+	  		arrival_time = arrival_data.split.pop
+	  		a_split = arrival_data.split
+	  		a_split.pop
+	  		arrival_airport = a_split.join(" ")
+	  		arrival_time = old_jb_time(date,arrival_time)
+	  		depart_time = old_jb_time(date,depart_time)
+	  		Flight.find_or_create_by_depart_time(trip_id: 6, airline_id: 1, depart_airport: depart_airport, depart_time: depart_time, arrival_airport: arrival_airport, arrival_time: arrival_time, seat_type: "COACH" )
+	  	end
+	end
   	#get messages from Jetblue and pick the html
   	jb_messages = account.messages.where(from: "reservations@jetblue.com", subject: "Itinerary for your upcoming trip")
   	jb_messages = jb_messages.map {|message| message.body_parts.first.content}
@@ -468,10 +491,64 @@ class PagesController < ApplicationController
   	account = contextio.accounts.where(email: 'blgruber@gmail.com').first
   	
   	#get messages from Virgin and pick the html
-  	u_messages = account.messages.where(from: "unitedairlines@united.com", subject: "/eTicket Itinerary and Receipt for Confirmation/i")
-  	u_oldest_messages = account.messages.where(from: "UNITED-CONFIRMATION@united.com")
-  	raise "#{u_oldest_messages.count}"
-  	u_messages = u_messages.map {|message| message.body_parts.first.content}
+  	#u_messages = account.messages.where(from: "unitedairlines@united.com", subject: "/eTicket Itinerary and Receipt for Confirmation/i")
+  	#u_oldest_messages = account.messages.where(from: "UNITED-CONFIRMATION@united.com", subject: "/Your United flight confirmation -/")
+  	u_oldest_messages = account.messages.where(from: "UNITED-CONFIRMATION@united.com", subject: '/Your United flight confirmation - January 27, 2011 - New York to Santa Barbara/')
+  	u_oldest_messages = u_oldest_messages.map {|message| message.body_parts.first.content}
+  	u_oldest_messages.each do |message|
+  		dom = Nokogiri::HTML(message)
+  		matches = dom.xpath('//*[@id="flightTable"]/tr').map(&:to_s)
+
+  		if matches.count.odd?
+  			odd_first_test = matches[4]
+  			odd_first_test = odd_first_test.gsub("&lt;","")
+  			odd_first_test = odd_first_test.gsub("&gt;","")
+  			connect = odd_first_test.scan(/<p>(.*?)<\/p>/).first.first
+  			if connect == " connecting to "
+  				raise "equal"
+  			else
+  				raise "not equal"
+  			end
+  		end
+  		matches = matches.each_slice(4).to_a
+  		matches.each do |flight|
+  			#strip = ActionView::Base.full_sanitizer.sanitize(flight[0])
+  			#split_date = strip.split[0].split(",")
+  			#month_day = split_date[1]
+  			#day = get_first_number(month_day)
+			#month = month_day.split("#{day}") 
+  			#month = month.first
+  			#year = get_first_number(split_date[2])
+			flight_data = flight[2].gsub("\t","")
+	  		flight_data = flight_data.gsub("\n","")
+	  		flight_data = flight_data.gsub("\r","")
+	  		seat_type = flight_data.scan(/<td style="padding-bottom:20px;">(.*?)<\/td>/).first.first
+  			flight_data = flight_data.scan(/<td>(.*?)<\/td>/)
+  			departure_data = flight_data[0].first.scan(/\>(.*?)\</)
+  			depart_airport = departure_data[0].first
+  			depart_hour = departure_data[1].first
+  			depart_time_data = departure_data[3].first.split
+  			depart_month = depart_time_data[1]
+  			depart_day = depart_time_data[2]
+  			depart_year = depart_time_data[3]
+  			depart_time = create_saveable_date(depart_day, depart_month, depart_year, depart_hour)
+
+  			arrival_data = flight_data[1].first.scan(/\>(.*?)\</)
+  			arrival_airport = arrival_data[0].first
+  			arrival_hour = arrival_data[1].first
+  			arrival_time_data = arrival_data[3].first.split
+  			arrival_month = arrival_time_data[1]
+  			arrival_day = arrival_time_data[2]
+  			arrival_year = arrival_time_data[3]
+  			arrival_time = create_saveable_date(arrival_day, arrival_month, arrival_year, arrival_hour)
+
+  			Flight.find_or_create_by_depart_time(trip_id: 48, airline_id: 83, depart_airport: depart_airport, depart_time: depart_time, arrival_airport: arrival_airport, arrival_time: arrival_time, seat_type: seat_type )
+
+  		end
+
+  		
+  	end
+
   end
 
   private
@@ -512,6 +589,7 @@ class PagesController < ApplicationController
   	#real_date = Chronic.parse(string_date)
   	return new_date
   end
+  
   def orbitz_time(string_date)
   	month_name = string_date.split[0]
   	month = Date::MONTHNAMES.index("#{month_name}")
@@ -519,4 +597,10 @@ class PagesController < ApplicationController
   	hour = "#{string_date.split[2]} #{string_date[3]}"
   	create_saveable_date(day, month, @year, hour)
   end
+
+  def old_jb_time(date,time)
+  	string_date = "#{date} #{time}"
+  	return Chronic.parse(string_date)
+  end
+
 end
