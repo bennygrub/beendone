83112b4705c5b6dcd65fad6c284d1a3f1440aadf
Ben Gruber <blgruber@gmail.com>
2014-09-25 18:55:51 +1000

    added line for local vs heroku

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,15 @@
 class AuthController < ApplicationController
   def connect
   	api = ContextIO.new('d67xxta6', 'AtuL8ONalrRJpQC0') #connect to contextio
-  	callback_url = "http://localhost:3000/auth/receive" #set call back url
+  	
+    if Rails.env.production?
+      callback_url = "http://ancient-citadel-8002.herokuapp.com/auth/receive" #set call back url
+    else
+      callback_url = "http://localhost:3000/auth/receive" #set call back url
+    end
   	response = api.connect_tokens.create(callback_url, options = {'email' => current_user.email})
-	#redirect for user to verify
-	redirect_to response.browser_redirect_url
+	 #redirect for user to verify
+	 redirect_to response.browser_redirect_url
 
   end
 
@@ -15,14 +20,6 @@ class AuthController < ApplicationController
   	uid = response.account.id
   	Authentication.create(user_id: current_user.id, provider: "contextio", uid: uid)
   	Resque.enqueue(SearchAll, current_user.id)
-    #Resque.enqueue(VirginGrab, current_user.id)
-  	#Resque.enqueue(JetblueGrab, current_user.id)
-  	#Resque.enqueue(CheapoGrab, current_user.id)
-  	#Resque.enqueue(DeltaGrab, current_user.id)
-  	#Resque.enqueue(UnitedGrab, current_user.id)
-  	#Resque.enqueue(OrbitzGrab, current_user.id)
-    #Resque.enqueue(FlighthubGrab, current_user.id)
-  	#Resque.enqueue(AaGrab, current_user.id)
   	flash[:notice] = "You're Map is being created, we will email you when you its ready"
   	redirect_to root_path
   end
