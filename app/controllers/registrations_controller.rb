c9a7b446964c48dca1813028b4d78162fd750c8b
Ben Gruber <blgruber@gmail.com>
2014-09-25 19:09:44 +1000

    found real spot to change

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,11 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     #auth_connect_path
     api = ContextIO.new('d67xxta6', 'AtuL8ONalrRJpQC0') #connect to contextio
-  	callback_url = "http://localhost:3000/auth/receive" #set call back url
+  	if Rails.env.production?
+      callback_url = "http://ancient-citadel-8002.herokuapp.com/auth/receive" #set call back url
+    else
+      callback_url = "http://localhost:3000/auth/receive" #set call back url
+    end
   	response = api.connect_tokens.create(callback_url, options = {'email' => current_user.email})
 	#redirect for user to verify
 	response.browser_redirect_url
