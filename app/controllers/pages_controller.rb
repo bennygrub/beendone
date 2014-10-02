22666cb83c84fe51bb8362d048b835cab8f096b5
Ben Gruber <blgruber@gmail.com>
2014-10-02 16:04:02 +1000

    adding US Airways before 2013 as well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -329,7 +329,52 @@ class PagesController < ApplicationController
   	contextio = ContextIO.new('d67xxta6', 'AtuL8ONalrRJpQC0')
   	#get the correct account
   	account = contextio.accounts.where(email: "blgruber@gmail.com").first
+  	email_change_date = Date.new(2014,1,1).to_time.to_i
   	a_id = Airline.where("name = ?", "USAir").first.id
+
+
+  	usa_messages = account.messages.where(from: "reservations@email-usairways.com", date_before: email_change_date)
+  	usa_messages.each do |message|
+	  	trip = Trip.find_or_create_by_message_id(user_id: user.id, message_id: message.message_id)
+  		dom = Nokogiri::HTML(message.body_parts.first.content)
+	  	matches = dom.xpath('/html/body/div/table/tr[2]/td/table/tr[1]/td/table[7]/tr')
+	  	important = matches.map{|match| match unless match.attributes["style"].blank?}.compact
+	  	split_points = important.map{|match| match if match.attributes["style"].value == "padding-top: 15px;"}.each_with_index.map{|a, index| index  unless a.nil?}.compact.map{|a| a unless a == 0}.compact
+	  	day_array = []
+	  	split_points.each_with_index do |split, index|
+	  		if split_points.count == 1
+	  			day_array << important[0...split]
+	  			day_array << important[split...important.count]
+	  		elsif index == 0#first iteration if there is more than 1 split point
+	  			day_array << important[0...split]
+	  		elsif index < split_points.count-1#2nd, 3rd, 4th iterations
+	  			day_array << important[split_points[index-1]...split]
+	  		else#last split going to end of array
+	  			day_array << important.each_slice(split).to_a.last
+	  		end
+	  	end
+	  	#day_array is array of the flights by day of travel
+	  	day_array.each do |day|
+	  		flight_count = (day.count-3) #counts the number of flights that day
+	  		date_month_day_year = day[1].to_s.gsub("\r", "").gsub("\n", "").gsub("\t","").gsub(%r{\"}, '').scan(/\/strong>(.*?)<\/div>/).first.first.strip!.gsub(",", "").split
+	  		flight_count_array = [*1..flight_count]
+	  		flight_count_array.each_with_index do |value, index|
+		  		y = (3)+(index*1)
+		  		binding.pry
+		  		depart_airport = Airport.find_by_faa(day[y].to_s.gsub("\r", "").gsub("\n", "").gsub("\t","").gsub(%r{\"}, '').scan(/<span style=color: #227db2;>(.*?)<\/span>/).first.first.split.first).id
+		  		arrival_airport = Airport.find_by_faa(day[y].to_s.gsub("\r", "").gsub("\n", "").gsub("\t","").gsub(%r{\"}, '').scan(/<span style=color: #227db2;>(.*?)<\/span>/).last.first.split.first).id
+		  		depart_time = am_pm_split(day[y].to_s.gsub("\r", "").gsub("\n", "").gsub("\t","").gsub(%r{\"}, '').scan(/td style=vertical-align: middle; margin: 0px; width: 80px; white-space: nowrap; text-align: center>(.*?)<span/).first.first.gsub(" ", ""))
+		  		arrival_time = am_pm_split(day[y].to_s.gsub("\r", "").gsub("\n", "").gsub("\t","").gsub(%r{\"}, '').scan(/td style=vertical-align: middle; margin: 0px; width: 80px; white-space: nowrap; text-align: center>(.*?)<span/).last.first.gsub(" ", ""))
+		  		#binding.pry
+		  		d_time = DateTime.new(date_month_day_year[3].to_i, month_to_number(date_month_day_year[1]).to_i, date_month_day_year[2].to_i, depart_time[:hour].to_i, depart_time[:min].to_i, 0, 0)
+		  		a_time = DateTime.new(date_month_day_year[3].to_i, month_to_number(date_month_day_year[1]).to_i, date_month_day_year[2].to_i, arrival_time[:hour].to_i, arrival_time[:min].to_i, 0, 0)
+
+		  		Flight.find_or_create_by_depart_time_and_trip_id(trip_id: trip.id, airline_id: a_id, depart_airport: depart_airport, depart_time: d_time, arrival_airport: arrival_airport, arrival_time: a_time, seat_type: "US Airways Before 2014" )
+		  	end
+
+	  	end
+  	end
+  	#US AIRWAYS NEW EMAIL STARTING 2014
   	#get messages from delta and pick the html
   	usa_messages = account.messages.where(from: "reservations@email-usairways.com")
   	usa_messages.each do |message|
@@ -370,33 +415,6 @@ class PagesController < ApplicationController
 		  	end
 
 	  	end
-  		
-
-  		#:bold;color:#277DB2;">(.*?)<\/span>
-  		#airport_array = message.scan(/:bold;color:#277DB2;">(.*?)<\/span>/)
-	  	#stripped = ActionView::Base.full_sanitizer.sanitize(message)
-	  	#stripped = stripped.gsub("\n","")
-	  	#stripped = stripped.gsub("\r","")
-	  	#stripped = stripped.gsub("\t","")
-	  	#stripped = stripped.gsub("&nbsp;","")
-	  	#raise "#{stripped}"
-
-	  	#fare = stripped.scan(/Subtotal(.*?)Number/).first.first
-	  	#departure_array =  stripped.scan(/DEPART(.*?)AIRCRAFT/)
-	  	
-	  	#weird_date_arrays = stripped.scan(/\bto\b(.*?)FLIGHT#/)
-	  	#date_arrays = weird_date_arrays.map{|full_date| full_date.first.split.last(3) }
-	  	
-	  	#raise "#{date_arrays}"
-	  	#binding.pry
-	  
-
-	  	
-	  	#raise "#{stripped.scan(/ARRIVE &nbsp; &nbsp;(.*?)&nbsp;/)}"
-
-
-
-
   	end
   end
   def jetblue
