38dcff5b83d692b4d63b539a0796c8c9983f4879
bennygrub <blgruber@gmail.com>
2014-10-16 08:08:05 -0400

    removed migration

--- db/migrate/20141016102128_change_airport_ids_to_integer.rb
+++
@@ -1,6 +0,0 @@
-class ChangeAirportIdsToInteger < ActiveRecord::Migration
-  def change
-  	change_column :flights, :arrival_airport, :integer
-  	change_column :flights, :depart_airport, :integer
-  end
-end
