0621c2b5c6f0ae96456f9122d78d1fb268d6b7bb
bennygrub <blgruber@gmail.com>
2014-10-16 07:52:48 -0400

    going back on migration

--- db/migrate/20141016102128_change_airport_ids_to_integer.rb
+++ db/migrate/20141016102128_change_airport_ids_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeAirportIdsToInteger < ActiveRecord::Migration
   def change
-  	change_column :flights, :arrival_airport, 'integer USING CAST(column_name AS integer)'
-  	change_column :flights, :depart_airport, 'integer USING CAST(column_name AS integer)'
+  	change_column :flights, :arrival_airport, :integer
+  	change_column :flights, :depart_airport, :integer
   end
 end
