e0ae8ff3b8dd2edaf394013b3832e7413b31fe86
bennygrub <blgruber@gmail.com>
2014-10-16 09:44:12 -0400

    integer now

--- db/migrate/20140703215533_create_flights.rb
+++ db/migrate/20140703215533_create_flights.rb
@@ -3,9 +3,9 @@ class CreateFlights < ActiveRecord::Migration
     create_table :flights do |t|
       t.integer :trip_id
       t.integer :airline_id
-      t.datetime :depart_airport
+      t.integer :depart_airport
       t.datetime :depart_time
-      t.datetime :arrival_airport
+      t.integer :arrival_airport
       t.datetime :arrival_time
       t.text :seat_type
 
