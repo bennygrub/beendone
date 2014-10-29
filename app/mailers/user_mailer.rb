b6411872028f9173e531c46ccebe49b5553e4292
Ben Gruber <blgruber@gmail.com>
2014-10-29 15:30:57 -0400

    apostrophe to quotes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,6 @@ class UserMailer < ActionMailer::Base
     @user = User.find(user)
     @trips = @user.trips.select{|trip| trip.flights.count > 0}
     #find a couple of fun stats to send with it
-    mail(to: "#{@user.name} <#{@user.email}>", subject: 'Your map is ready: we found #{@trips.count} of your trips')
+    mail(to: "#{@user.name} <#{@user.email}>", subject: "Your map is ready: we found #{@trips.count} of your trips")
   end
 end
